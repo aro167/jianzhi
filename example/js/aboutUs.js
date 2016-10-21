f867360835cc863be09efa3d9fb8cb8b23aab582
刘金梦 <1374475796@qq.com>
2016-10-22 02:42:22 +0800

    添加腕表页

--- example/js/aboutUs.js
+++ example/js/aboutUs.js
@@ -19,7 +19,18 @@ $(function(){
 			$(".showSearch").focus();
 		}, 1000);
 	});
-	$(".closeSearch").on('click',function(){
+	$(".xxx").click(function(){
+		$('#dowebok').addClass('bounce');
+		$("nav>ul>li").each(function(){
+			$(this).removeClass("animated fadeOutLeft");
+			$(this).addClass("animated fadeInLeft");
+		});
+		
+		
+		$('.search').show();
+		$('.inputdis').hide();
+	});
+	/*$(".closeSearch").on('click',function(){
 		if($(".showSearch").val() != ""){
 			$("nav>ul>li").each(function(){
 				$(this).removeClass("animated fadeOutLeft");
@@ -37,7 +48,7 @@ $(function(){
 		
 		$('.search').show();
 		$('.inputdis').hide();
-	});
+	});*/
 	//搜索按钮
 	$(".searchAddress").on('click',function(){
 		$('.searchBox').show();
@@ -46,4 +57,6 @@ $(function(){
 		$('.default').hide();
 		$('.searchResult').show();
 	});
+	$(".aboutWatch").css('margin-top',$(window).width()*0.131-160);
+	/*$(".aboutWatch").height($(window).width()*0.5625-160);*/
 })
